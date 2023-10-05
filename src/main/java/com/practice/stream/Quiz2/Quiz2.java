a2b5590ade35df9f0e9c5bf454c28faabd6b37e1
jiwon615 <seg615@naver.com>
2023-10-05 20:14:20 +0900

    feat: quiz2.1 다른 방식으로 풀기

--- src/main/java/com/practice/stream/Quiz2/Quiz2.java
+++ src/main/java/com/practice/stream/Quiz2/Quiz2.java
@@ -9,7 +9,7 @@ public class Quiz2 {
 
     private static List<String> WORDS = Arrays.asList("TONY", "a", "hULK", "B", "america", "X", "nebula", "Korea");
 
-    // 2.1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
+    // 2.1 방식1 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
     // ex) ("T", 1), ("a", 2) ...
     public Map<String, Integer> quiz1() {
         Map<String, Integer> result = WORDS.stream()
@@ -17,15 +17,23 @@ public class Quiz2 {
         return result;
     }
 
+    // 2.1 방식2 List에 저장된 단어들의 접두사가 각각 몇개씩 있는지 Map<String, Integer>으로 변환하여라.
+    // ex) ("T", 1), ("a", 2) ...
+    public Map<String, Integer> quiz1_secondTry() {
+        Map<String, Integer> result = WORDS.stream()
+                .collect(Collectors.toMap(word -> String.valueOf(word.charAt(0)), word -> 1, Integer::sum));
+        return result;
+    }
+
+
     // 2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 각 첫 글자만 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.
     // ex) ["Hello", "a", "Island", "b"] -> “H I”
     public String quiz2() {
         String result = WORDS.stream()
-                .filter(line -> line.length() > 1)
+                .filter(word -> word.length() > 1)
                 .map(String::toUpperCase)
-                .map(line -> String.valueOf(line.charAt(0)))
+                .map(word -> String.valueOf(word.charAt(0)))
                 .collect(Collectors.joining(" "));
         return result;
     }
-
 }
--- src/test/java/com/practice/stream/Quiz2/Quiz2Test.java
+++ src/test/java/com/practice/stream/Quiz2/Quiz2Test.java
@@ -14,12 +14,13 @@ class Quiz2Test {
     @Test
     void quiz1() {
         // {"B"=1, "K"=1, "T"=1, "X"=1, "a"=2, "h"=1, "n"=1}
-        assertThat(quiz.quiz1()).isEqualTo(answer.quiz1());
+        assertThat(quiz.quiz1_secondTry()).isEqualTo(answer.quiz1());
     }
 
     @DisplayName("2.2 List에 저장된 단어들 중에서 단어의 길이가 2 이상인 경우에만, 모든 단어를 대문자로 변환하여 각 첫 글자만 스페이스로 구분한 하나의 문자열로 합한 결과를 반환하여라.")
     @Test
     void quiz2() {
+        // T H A N K
         assertThat(quiz.quiz2()).isEqualTo(answer.quiz2());
     }
 
