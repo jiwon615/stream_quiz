d0c6b1ad3af3ef4760853a3373870cd207c435af
jiwon615 <seg615@naver.com>
2023-10-24 20:14:20 +0900

    comment: quiz6 주석 수정

--- src/main/java/com/practice/stream/Quiz6/Quiz6.java
+++ src/main/java/com/practice/stream/Quiz6/Quiz6.java
@@ -31,12 +31,12 @@ public class Quiz6 {
         };
     }
 
-    // stuArr에서 불합격(150점 미만)한 학생의 수를 남자와 여자로 구별하여라. (Boolean, List)
+    // 6.1 stuArr에서 불합격(150점 미만)한 학생의 수를 남자와 여자로 구별하여라. (Boolean, List)
     public Map<Boolean, List<Student>> quiz1() {
         return new HashMap<>();
     }
 
-    // 각 반별 총점을 학년 별로 나누어 구하여라 (Map<Integer, Map<Integer, Integer>>)
+    // 6.2 각 반별 총점을 학년 별로 나누어 구하여라 (Map<Integer, Map<Integer, Integer>>)
     public Map<Integer, Map<Integer, Integer>> quiz2() {
         return new HashMap<>();
     }
